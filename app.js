105002906c2a0d3503278c75af50a315f49fdca6
guavalines <konstrictor@gmail.com>
2023-05-26 23:49:56 -0700

    added loop

--- app.js
+++ app.js
@@ -3,4 +3,14 @@ const search = () =>{
   const storeitems = document.getElementById("product-list")
   const product = document.querySelectorAll(".product")
   const pname = document.getElementsByTagName("h2")
+
+  for(var i=0; i < pname.length; i++){
+    let match = product[i].getElementByTagName('h2')[0];
+
+    if(match){
+      let textvalue = match.textContent || match.innerHTML
+
+      if(textvalue.toUpperCase().indexOf(searchbox) > -1)
+    }
+  }
 }
