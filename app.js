bb7b431b02a439cadeb63b2f3faa03086adfa3ed
guavalines <konstrictor@gmail.com>
2023-05-26 23:40:31 -0700

    coded search function

---
+++ app.js
@@ -0,0 +1,6 @@
+const search = () =>{
+  const searchbox = document.getElementById("search-item").ariaValueMax.toUpperCase();
+  const storeitems = document.getElementById("product-list")
+  const product = document.querySelectorAll(".product")
+  const pname = document.getElementsByTagName("h2")
+}
