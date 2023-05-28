1d308be93921e7926637b34b662625a6f4141d1c
guavalines <konstrictor@gmail.com>
2023-05-27 23:29:33 -0700

    finished conditional statement

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const search = () =>{
     if(match){
       let textvalue = match.textContent || match.innerHTML
 
-      if(textvalue.toUpperCase().indexOf(searchbox) > -1)
+      if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+        product[i].style.display = "";
+      }else{
+        product[i].style.display = "none";
+      }
     }
   }
 }
